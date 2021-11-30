65512134c4176e4766f2d7b3f50d11e0e64e17cf
minketmon <gjm1026@naver.com>
2021-12-01 00:42:32 +0900

    python

--- pgm_오랜기간보호한동물들(1).sql
+++
@@ -1,2 +0,0 @@
-SELECT i.name, i.datetime from animal_ins i left join animal_outs o on i.animal_id=o.animal_id
-where o.animal_id is null order by i.datetime limit 3;
--- pgm_입양시각구하기(1).sql
+++
@@ -1,3 +0,0 @@
-select hour(datetime) as hour, count(hour(Datetime)) as count from animal_outs
-where hour(datetime)>=9 and hour(datetime)<20
-group by hour order by hour;
